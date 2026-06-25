5c59f20952315a324e6dad276785397b95273f54
andrjre <andrjre@gmail.com>
2026-06-25 14:19:55 +0100

    Add and view functions

--- todoList.cpp
+++ todoList.cpp
@@ -3,6 +3,44 @@
 
 int main(){
 
-    std::cout << "Todo List" << '\n';
+    std::string action = "";
+    std::vector<std::string> taskList;
+
+    std::cout << "      | Todo List |     " << '\n';
+
+    while(action != "exit"){
+        
+        // initial prompt
+        std::cout << "What would you like to do?" << '\n';
+        std::cout << "View    " << "Add    " << "Remove    " << "Exit    " << '\n';
+        std::cin >> action;
+
+        if(action == "add"){
+            std::cout << "Name of task: ";
+            std::string taskName = "";
+            std:: cin >> taskName;
+
+            if(taskName != ""){
+                taskList.push_back(taskName);
+                std::cout << "Task added!" << '\n';
+            }
+            action = "";
+        }
+
+
+        else if(action == "view"){
+
+            if(taskList.empty() == 1){
+                std::cout << "No tasks" << '\n';
+            }
+            else{
+                for(int i = 0; i < taskList.size(); i++){
+                    std::cout << "[] " << taskList[i] << '\n';
+                }
+            }
+
+        }
+
+    }
 
 }   
